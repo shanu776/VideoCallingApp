c25ae0fce8e865b7d90f36f092a360a56f4e0f13
Bhanu Pratap Singh <gokusupersayin9@gmail.com>
2025-03-10 21:23:05 +0530

    Update MainActivity.java

--- app/src/main/java/callinapp/example/com/videocallingapp/MainActivity.java
+++ app/src/main/java/callinapp/example/com/videocallingapp/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements Connector.IConnec
 
     }
     public void connect(View v){
-        String token = "cHJvdmlzaW9uAGJoYW51QDM5OTRkOS52aWR5by5pbwA2MzcxMTQ5OTU0OAAAODE4ZTEwOWU0YWIxYTk0ZWJhMmU3ZGI2YTRlMTM3NmEwY2EwYTg3NjA1YzdhNWFlMWVmNDg2ZmY5NjA2Y2M4YWRhNmU5NDg0MTQ0MGRkMWIyMTc5YjY1ZDI1YTlhNzEy";
+        String token = "";
         vc.connect("prod.vidyo.io",token,"User", "DemoRoom", this);
     }
     public void disconnect(View v){
